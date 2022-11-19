c0a995b5e44dc66a415b9409d69756e6e09bb228
Matheus Alencar <matlopes96@outlook.com>
2022-11-18 22:53:51 -0300

    Adicionei mensagem de "campo obrigatório"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
+const nomeDoUsuario = document.getElementById("user-name");
+const emailDoUsuario = document.getElementById("user-email");
+const telefoneDoUsuario = document.getElementById("user-number");
 const mensagemDoUsuario = document.getElementById("user-message");
+const botaoEnviar = document.getElementById("botao-enviar");
+const mensagensCampoObrigatorio = document.getElementsByClassName("mensagem-campo-obrigatorio");
+
+
+function checarPreenchimento(input, array, index) {
+    if(input.value == "") {
+        input.style.border = "1px solid #ff0000";
+        array[index].style.display = "inline";
+    } else {
+        input.style.border = "1px solid #008000";
+        array[index].style.display = "none";
+    }
+}
+
+botaoEnviar.addEventListener("click",
+    () => {
+        checarPreenchimento(nomeDoUsuario, mensagensCampoObrigatorio, 0);
+        checarPreenchimento(emailDoUsuario, mensagensCampoObrigatorio, 1);
+        checarPreenchimento(telefoneDoUsuario, mensagensCampoObrigatorio, 2);
+        checarPreenchimento(mensagemDoUsuario, mensagensCampoObrigatorio, 3);
+    }
+)
 
 
 
